f67ca3cc3ecfeac5c795b6e21863037990b76ab5
omi <k_omi@miraiyacreation.co.jp>
2021-10-06 00:06:42 +0900

    counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "./app.css";
+// import styles from "./app.module.css";
 function App() {
-  return <div className="App">nankakaku</div>;
+  const [count, setCout] = useState(0);
+
+  //増えるは5まで
+  //減るは0まで
+  console.log(count);
+  return (
+    <div>
+      <div className="App">{count}</div>
+      <button
+        type={"button"}
+        disabled={count >= 5}
+        onClick={() => {
+          setCout((prevState) => prevState + 1);
+        }}
+      >
+        増える
+      </button>
+      <button
+        type={"button"}
+        disabled={count <= 0}
+        onClick={() => {
+          setCout((prevState) => prevState - 1);
+        }}
+      >
+        減る
+      </button>
+      <button
+        type={"button"}
+        onClick={() => {
+          setCout(0);
+        }}
+      >
+        リセット
+      </button>
+    </div>
+  );
 }
 
 export default App;
