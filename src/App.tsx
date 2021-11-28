6c3f6c9813deb75a9cb9610badb130ce2d144c4e
omikana <0123kana0123@gmail.com>
2021-11-28 23:11:52 +0900

    app push

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {requests}  from "./libs/request";
+import {instance}  from "./libs/axios";
+
+type Movie = {
+  id: string;
+  name: string;
+  title: string;
+  original_name: string;
+  poster_path: string;
+  backdrop_path: string;
+};
 
 function App() {
-  return <div className="App">nankakaku</div>;
+  const [moveis,setMoveis] =useState<Movie[]>([])
+
+  useEffect(()=>{
+    (async()=>{
+      const res = await instance.get<{results:Movie[]}>(requests.fetchTrending)
+      setMoveis(res.data.results)
+    })()
+  },[])
+
+  console.log(moveis);
+  return <div className="App">{moveis.map((movie)=>{
+    return (
+      <div key="movie.id">
+        <div>{movie.title}</div>
+        <img src={"https://image.tmdb.org/t/p/original" + movie.backdrop_path} alt="" />
+      </div>
+    )
+  })}</div>;
+
 }
 
 export default App;
